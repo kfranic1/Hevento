dc87551ceb9b3866775c510c068e12da57033e0e
Filip Begović <filip.begi@gmail.com>
2022-05-04 15:46:54 +0200

    Registration form design done

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,77 +1,186 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hevento/model/person.dart';
 import 'package:hevento/routing/custom_router_delegate.dart';
 import 'package:hevento/services/auth_service.dart';
+import 'package:hevento/services/constants.dart';
 import 'package:provider/provider.dart';
 
-class RegisterPage extends StatelessWidget {
-  RegisterPage({Key? key}) : super(key: key);
+// ignore: must_be_immutable
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
 
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  String? error;
+  late bool _passwordVisible;
+
+  @override
+  void initState() {
+    _passwordVisible = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     Person? appUser = Provider.of<Person?>(context);
-    return Scaffold(
-      body: Center(
-        child: appUser != null
-            ? const Center(child: Text("Already signed in"))
-            : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Form(
-                    child: SizedBox(
-                      width: 500,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text("Sign Up"),
-                          TextFormField(
-                            decoration: const InputDecoration(hintText: "full name"),
-                            controller: _nameController,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(hintText: "email"),
-                            controller: _emailController,
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(hintText: "username"),
-                            controller: _usernameController,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(hintText: "password"),
-                            controller: _passwordController,
-                            obscureText: true,
-                          ),
-                          const SizedBox(height: 20),
-                          Center(
-                            child: ElevatedButton(
-                              onPressed: () async {
-                                Person newPerson = Person('');
-                                newPerson.name = _nameController.text;
-                                newPerson.email = _emailController.text;
-                                newPerson.username = _usernameController.text;
-                                await Provider.of<AuthService>(context, listen: false).signUp(
-                                  _emailController.text,
-                                  _passwordController.text,
-                                  newPerson,
-                                );
-                                context.read<CustomRouterDelegate>().goToHome();
-                              },
-                              child: const Text("Sign Up as User"),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Center(
+          child: appUser != null
+              ? const Center(child: Text("Already signed in"))
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (constraints.maxWidth > kNarrow)
+                      Expanded(
+                        child: Image.asset(
+                          'images/registerImage.jpg',
+                          fit: BoxFit.cover,
+                          height: MediaQuery.of(context).size.height,
+                        ),
+                      ),
+                    if (constraints.maxWidth > kNarrow) const SizedBox(width: 50),
+                    Expanded(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        child: Form(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Dobro došli !",
+                                  style: titleStyle,
+                                ),
+                                const Text("Kreiraj svoj profil", style: subTitleStyle),
+                                const SizedBox(height: 100),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width * (constraints.maxWidth > kNarrow ? 0.35 : 0.7),
+                                  child: TextFormField(
+                                    cursorColor: darkGreen,
+                                    decoration: InputDecoration(
+                                        hintText: "Ime i prezime: ",
+                                        hintStyle: const TextStyle(fontWeight: FontWeight.bold),
+                                        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0)),
+                                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0))),
+                                    controller: _nameController,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width * (constraints.maxWidth > kNarrow ? 0.35 : 0.7),
+                                  child: TextFormField(
+                                    cursorColor: darkGreen,
+                                    decoration: InputDecoration(
+                                        hintText: "email: example@ex.com",
+                                        hintStyle: const TextStyle(fontWeight: FontWeight.bold),
+                                        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0)),
+                                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0))),
+                                    controller: _emailController,
+                                    keyboardType: TextInputType.emailAddress,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width * (constraints.maxWidth > kNarrow ? 0.35 : 0.7),
+                                  child: TextFormField(
+                                    cursorColor: darkGreen,
+                                    decoration: InputDecoration(
+                                        hintText: "Korisničko ime",
+                                        hintStyle: const TextStyle(fontWeight: FontWeight.bold),
+                                        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0)),
+                                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0))),
+                                    controller: _usernameController,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width * (constraints.maxWidth > kNarrow ? 0.35 : 0.7),
+                                  child: TextFormField(
+                                    obscureText: !_passwordVisible,
+                                    cursorColor: darkGreen,
+                                    decoration: InputDecoration(
+                                      hintText: "Lozinka",
+                                      hintStyle: const TextStyle(fontWeight: FontWeight.bold),
+                                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0)),
+                                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: darkGreen, width: 2.0)),
+                                      suffixIcon: IconButton(
+                                        icon: Icon(
+                                          _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                                          color: Colors.grey,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            _passwordVisible = !_passwordVisible;
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                    controller: _passwordController,
+                                  ),
+                                ),
+                                const SizedBox(height: 150),
+                                Container(
+                                  height: MediaQuery.of(context).size.height * 0.05,
+                                  width: max(MediaQuery.of(context).size.width * 0.1, 100),
+                                  decoration: BoxDecoration(color: darkGreen, borderRadius: BorderRadius.circular(10)),
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      Person newPerson = Person('');
+                                      newPerson.name = _nameController.text;
+                                      newPerson.email = _emailController.text;
+                                      newPerson.username = _usernameController.text;
+                                      String? result = await Provider.of<AuthService>(context, listen: false).signUp(
+                                        _emailController.text,
+                                        _passwordController.text,
+                                        newPerson,
+                                      );
+                                      if (result == null) context.read<CustomRouterDelegate>().goToHome();
+                                      setState(() {
+                                        error = result;
+                                      });
+                                      context.read<CustomRouterDelegate>().goToHome();
+                                    },
+                                    child: const Text(
+                                      "Registriraj se",
+                                      style: buttonTxtStyle,
+                                    ),
+                                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(darkGreen)),
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                if (error != null) Text(error!),
+                              ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-      ),
-    );
+                  ],
+                ),
+        ),
+      );
+    });
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
